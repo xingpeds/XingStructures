43b5bb6273fb79716a636ae77ca081d8c3a256da
Kyle McBurnett <kylemcburnett@xingpeds.com>
2022-12-11 21:31:41 -0800

    I ❤️ kotest.

--- src/commonTest/kotlin/convertions/ToSuspenddableListTest.kt
+++ src/commonTest/kotlin/convertions/ToSuspenddableListTest.kt
@@ -2,7 +2,10 @@ package convertions
 
 import com.xingpeds.collectionsus.convertions.toSuspendableList
 import io.kotest.property.Arb
+import io.kotest.property.PropertyContext
+import io.kotest.property.arbitrary.int
 import io.kotest.property.arbitrary.list
+import io.kotest.property.arbitrary.orNull
 import io.kotest.property.arbitrary.string
 import io.kotest.property.forAll
 import kotlin.test.Test
@@ -13,14 +16,29 @@ import kotlinx.coroutines.test.runTest as runCoTest
 class ToSuspenddableListTest {
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun aList() = runCoTest {
-        forAll(Arb.list(Arb.string())) { list ->
+    fun nonNullGetOperator() = runCoTest {
+        fun <T> compareLists(): suspend PropertyContext.(List<T>) -> Boolean = { list ->
             val suspendList = list.toSuspendableList()
             var result = true
             for (i in (0..list.lastIndex)) {
                 result = result && (list[i] == suspendList[i].first())
             }
             result
+        }
+        forAll(Arb.list(Arb.string()), compareLists<String>())
+        forAll(Arb.list(Arb.int()), compareLists<Int>())
+    }
+
+    @Test
+    fun nullableGetOperator() = runCoTest {
+        forAll(Arb.list(Arb.int().orNull())) { list ->
+            val suspendList = list.toSuspendableList()
+            for (i in (0..list.lastIndex)) {
+                if (list[i] != suspendList[i].first()) {
+                    return@forAll false
+                }
+            }
+            true
         }
     }
 }
